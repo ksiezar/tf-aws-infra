8d60062e0b5f33de0235dbf5787535e907971028
KatherineSiezar-Arroyo <katherine_siezararroyo@cargill.com>
2022-07-15 11:15:28 -0600

    TF testing

--- tf-aws/main.tf
+++ tf-aws/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "windows_server" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}"
-  network_interface_id = "${var.vpc_id}"
-  vpc_security_group_ids = "${var.vpc_security_group}"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  availability_zone      = var.aws_region
+  network_interface_id   = var.vpc_id
+  vpc_security_group_ids = var.vpc_security_group
 
   tags {
     Name = "windows-main"
--- tf-aws/variables.tf
+++ tf-aws/variables.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "vpc_security_group" {
-  type = string
+  type    = string
   default = "sg-0359e36425359a42d"
 }
 
 variable "vpc_id" {
-  type = string
+  type    = string
   default = "vpc-03a30cf3ae0d09353"
 }
 
 variable "vpc_cidr_block" {
-  type = string
+  type    = string
   default = "172.31.0.0/16"
 }
 
-variable "ami" {
-    type = string
-    default = "ami-041306c411c38a789"
+variable "ami_id" {
+  type    = string
+  default = "ami-041306c411c38a789"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
