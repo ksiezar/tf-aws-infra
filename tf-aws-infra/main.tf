921449e6de4caba95c15bc3a17a1a6f3bf4a1608
KatherineSiezar-Arroyo <katherine_siezararroyo@cargill.com>
2022-07-16 23:25:02 -0600

    Fixing infra

--- tf-aws-infra/main.tf
+++ tf-aws-infra/main.tf
@@ -83,13 +83,9 @@ resource "aws_instance" "rhel_server" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.vms_key
-
+  security_groups        = [aws_security_group.launch-wizard-1.name]
   tags = {
     Name = "slave-01"
   }
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment_instance" {
-  security_group_id    = aws_security_group.launch-wizard-1.id
-  network_interface_id = aws_instance.rhel_server.primary_network_interface_id
-}
