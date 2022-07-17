e4af9e54ae8bfee85610f6d1a5b117ed6bdc9d13
KatherineSiezar-Arroyo <katherine_siezararroyo@cargill.com>
2022-07-17 16:36:08 -0600

    Testing terraform infra

--- tf-aws-infra/main.tf
+++ tf-aws-infra/main.tf
@@ -2,20 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-###### VPC ID ######
-
-resource "aws_vpc" "main" {
-  cidr_block = "172.31.0.0/16"
-}
-
 ###### Security Group ######
 
-resource "aws_security_group" "sg_instances" {
-  name        = "sg_instances"
+resource "aws_security_group" "sg_instance_vms" {
+  name        = "sg_instance_vms"
   description = "Allow Application inbound traffic"
   vpc_id      = var.vpc_id
   tags = {
-    Name = "sg_instances"
+    Name = "sg_instance_vms"
   }
 }
 
@@ -57,15 +51,6 @@ resource "aws_security_group_rule" "ssh-ingress" {
   security_group_id = aws_security_group.sg_instances.id
 }
 
-resource "aws_security_group_rule" "application-ingress" {
-  type              = "ingress"
-  from_port         = 3000
-  to_port           = 3000
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sg_instances.id
-}
-
 ###### Security Group Rules Outbound ######
 
 resource "aws_security_group_rule" "app-outbound" {
@@ -78,16 +63,6 @@ resource "aws_security_group_rule" "app-outbound" {
 }
 
 ###### EC2 Instances ######
-
-resource "aws_instance" "rhel_server" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.vms_key
-  security_groups        = [aws_security_group.sg_instances.name]
-  tags = {
-    Name = "slave-01"
-  }
-}
 
 resource "aws_instance" "rhel_server02" {
   ami                    = var.ami_id
